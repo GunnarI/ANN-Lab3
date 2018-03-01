f26c64a562a04d1d97b78b04b5eaed5e3a85c24e
sinorious <erndwein@kth.se>
2018-03-01 10:35:42 +0100

    Newest version

--- recall.m
+++ recall.m
@@ -1,16 +1,19 @@
-function output = recall(input,n_patterns,matrix_size,W)
+function [output,energy] = recall(input,n_patterns,matrix_size,W)
 %%% update rule for recalling of the function
-
+energy = zeros(n_patterns,matrix_size(2));
 output = zeros(n_patterns,matrix_size(2));
 for n = 1:n_patterns
     for i = 1:matrix_size(2)
         weight = 0;
+        sum = 0;
             for j = 1:matrix_size(2)
                 weight = W(i,j) * input(n,j)+weight;
-                % look at distorted input patterns
+                %calculating energy
+                sum = W(i,j)*output(i)*output(j) + sum;
             end
         output(n,i) = sign(weight);
+        energy(n,i) = -sum;
     end
+    
 end
-
 end
